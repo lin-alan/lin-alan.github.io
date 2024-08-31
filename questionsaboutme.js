33cb826ba81ce5037a0f1b71de78dbbf86262174
ALAN LIN <80351845+lin-alan@users.noreply.github.com>
2024-08-31 23:56:42 +0900

    added project links

---
+++ questionsaboutme.js
@@ -0,0 +1,7 @@
+// document.getElementById('aboutme').addEventListener('click');
+// const season = prompt('What is my favourite season?');
+
+// if (season === 'winter' || 'autumn' || 'spring') {
+//     console.log("Nope, It's in correct");
+
+// }
